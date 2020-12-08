fc26970edf503f5d501bd904558444dfd139efec
Matt Gill <MattGill98@users.noreply.github.com>
2020-12-08 14:35:54 +0000

    FISH-796 Merge pull request #5012 from flowlogix/Fix-Clustered-Singleton-Bugs
    FISH-796 Fix clustered singleton bugs and added test

--- appserver/tests/payara-samples/samples/clustered-singleton/clustered-singleton-test/src/main/java/fish/payara/samples/clustered/singleton/ClusteredSingletonInterceptedEJB.java
+++ appserver/tests/payara-samples/samples/clustered-singleton/clustered-singleton-test/src/main/java/fish/payara/samples/clustered/singleton/ClusteredSingletonInterceptedEJB.java
@@ -1,7 +1,11 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
+<<<<<<< HEAD
  * Copyright (c) [2016-2021] Payara Foundation and/or its affiliates. All rights reserved.
+=======
+ * Copyright (c) [2016-2020] Payara Foundation and/or its affiliates. All rights reserved.
+>>>>>>> a62d616b29 (Merge pull request #5012 from flowlogix/Fix-Clustered-Singleton-Bugs)
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
