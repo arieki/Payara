50f76a81246cd65684e9fb64b3c0020466c38e95
Matt Gill <MattGill98@users.noreply.github.com>
2021-01-14 11:06:32 +0000

    Merge pull request #4997 from sgflt/4653-fixed-virtual-server-config-propagation
    FISH-765 #4653 Propagate VirtualServer config to WebModule

--- appserver/web/web-glue/src/main/java/com/sun/enterprise/web/WebContainer.java
+++ appserver/web/web-glue/src/main/java/com/sun/enterprise/web/WebContainer.java
@@ -1926,7 +1926,7 @@ public class WebContainer implements org.glassfish.api.container.Container, Post
         webModule.setIasWebAppConfigBean(iasBean);
 
         // Configure SingleThreadedServletPools, work/tmp directory etc
-        webModule.configureMiscSettings(iasBean, virtualServer, displayContextPath);
+        webModule.configureMiscSettings(virtualServer, displayContextPath);
 
         // Configure alternate docroots if dummy web module
         if (webModule.getID().startsWith(DEFAULT_WEB_MODULE_NAME)) {
