97f71c628fe5d5a199cd9c1658866c9f40651c40
Andrew Pielage <pandrex247@hotmail.com>
2021-02-17 17:41:05 +0000

    FISH-758 Move synthetic FT interceptor into individual interceptors.
    Got Retry and other annotations to get detected for Rest Client.
    Asynchronous throws an exception however during deployment so is
    commented out, and not sure how to tackle Fallback due to its
    special nature of not allowing targets of TYPE (class).
    
    Signed-off-by: Andrew Pielage <pandrex247@hotmail.com>

--- appserver/payara-appserver-modules/microprofile/fault-tolerance/src/main/java/fish/payara/microprofile/faulttolerance/cdi/FaultToleranceInterceptor.java
+++ appserver/payara-appserver-modules/microprofile/fault-tolerance/src/main/java/fish/payara/microprofile/faulttolerance/cdi/FaultToleranceInterceptor.java
@@ -39,34 +39,27 @@
  */
 package fish.payara.microprofile.faulttolerance.cdi;
 
+import fish.payara.microprofile.faulttolerance.FaultToleranceConfig;
+import fish.payara.microprofile.faulttolerance.FaultToleranceService;
+import fish.payara.microprofile.faulttolerance.policy.FaultTolerancePolicy;
+import fish.payara.microprofile.faulttolerance.service.Stereotypes;
+import org.eclipse.microprofile.faulttolerance.exceptions.FaultToleranceDefinitionException;
+import org.glassfish.internal.api.Globals;
+
+import javax.enterprise.context.control.RequestContextController;
+import javax.enterprise.inject.Instance;
+import javax.enterprise.inject.spi.BeanManager;
+import javax.inject.Inject;
+import javax.interceptor.InvocationContext;
 import java.io.Serializable;
 import java.lang.annotation.Annotation;
+import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Supplier;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import javax.annotation.Priority;
-import javax.enterprise.context.control.RequestContextController;
-import javax.enterprise.inject.Instance;
-import javax.enterprise.inject.spi.BeanManager;
-import javax.inject.Inject;
-import javax.interceptor.AroundInvoke;
-import javax.interceptor.Interceptor;
-import javax.interceptor.InvocationContext;
-
-import org.eclipse.microprofile.faulttolerance.exceptions.FaultToleranceDefinitionException;
-import org.glassfish.internal.api.Globals;
-
-import fish.payara.microprofile.faulttolerance.FaultToleranceConfig;
-import fish.payara.microprofile.faulttolerance.FaultToleranceService;
-import fish.payara.microprofile.faulttolerance.policy.FaultTolerancePolicy;
-import fish.payara.microprofile.faulttolerance.service.Stereotypes;
-
-@Interceptor
-@FaultTolerance
-@Priority(Interceptor.Priority.PLATFORM_AFTER + 15)
 public class FaultToleranceInterceptor implements Stereotypes, Serializable {
 
     private static final Logger logger = Logger.getLogger(FaultToleranceInterceptor.class.getName());
@@ -77,8 +70,13 @@ public class FaultToleranceInterceptor implements Stereotypes, Serializable {
     @Inject
     private Instance<RequestContextController> requestContextControllerInstance;
 
-    @AroundInvoke
+    private FaultToleranceService faultToleranceService;
+
+    protected static final String PAYARA_FAULT_TOLERANCE_INTERCEPTOR_EXECUTED =
+            "fish.payara.microprofile.faulttolerance.executed";
+
     public Object intercept(InvocationContext context) throws Exception {
+        context.getContextData().put(PAYARA_FAULT_TOLERANCE_INTERCEPTOR_EXECUTED, Boolean.TRUE);
         try {
             FaultToleranceService env =
                     Globals.getDefaultBaseServiceLocator().getService(FaultToleranceService.class);
@@ -109,6 +107,17 @@ public class FaultToleranceInterceptor implements Stereotypes, Serializable {
     @Override
     public Set<Annotation> getStereotypeDefinition(Class<? extends Annotation> stereotype) {
         return beanManager.getStereotypeDefinition(stereotype);
+    }
+
+    protected boolean shouldIntercept(InvocationContext invocationContext) throws Exception {
+        Map<String, Object> contextData = invocationContext.getContextData();
+
+        if (contextData.get(PAYARA_FAULT_TOLERANCE_INTERCEPTOR_EXECUTED) != null &&
+                (Boolean) contextData.get(PAYARA_FAULT_TOLERANCE_INTERCEPTOR_EXECUTED)) {
+            return false;
+        }
+
+        return true;
     }
 
 }
