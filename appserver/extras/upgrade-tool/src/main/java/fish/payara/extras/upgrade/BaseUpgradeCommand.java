e6ca4fa23e23c5ab76ab67981ad50f5136096a2f
Andrew Pielage <pandrex247@hotmail.com>
2021-02-22 15:28:01 +0000

    FISH-962 Copyright and comment
    Signed-off-by: Andrew Pielage <pandrex247@hotmail.com>

--- appserver/extras/upgrade-tool/src/main/java/fish/payara/extras/upgrade/BaseUpgradeCommand.java
+++ appserver/extras/upgrade-tool/src/main/java/fish/payara/extras/upgrade/BaseUpgradeCommand.java
@@ -1,3 +1,43 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (c) 2020-2021 Payara Foundation and/or its affiliates. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License.  You can
+ * obtain a copy of the License at
+ * https://github.com/payara/Payara/blob/master/LICENSE.txt
+ * See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at glassfish/legal/LICENSE.txt.
+ *
+ * GPL Classpath Exception:
+ * The Payara Foundation designates this particular file as subject to the "Classpath"
+ * exception as provided by the Payara Foundation in the GPL Version 2 section of the License
+ * file that accompanied this code.
+ *
+ * Modifications:
+ * If applicable, add the following below the License Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyright [year] [name of copyright owner]"
+ *
+ * Contributor(s):
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+
 package fish.payara.extras.upgrade;
 
 import com.sun.enterprise.admin.cli.CLICommand;
@@ -36,6 +76,9 @@ import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/**
+ * Base class containing shared methods and variables used by other upgrade/rollback commands.
+ */
 public abstract class BaseUpgradeCommand extends LocalDomainCommand {
 
     protected static final Logger LOGGER = Logger.getLogger(CLICommand.class.getPackage().getName());
@@ -60,11 +103,17 @@ public abstract class BaseUpgradeCommand extends LocalDomainCommand {
         // Perform usual validation; we don't want to skip it or alter it in anyway, we just want to add to it.
         super.validate();
 
+        // Set up the install dir variable
         glassfishDir = getDomainsDir().getParent();
 
+        // Initialise the MOVEFOLDERS variable
         initialiseMoveFolders();
     }
 
+    /**
+     * Initialises the MOVEFOLDERS variable using the upgrade-tool.properties file.
+     * @throws CommandException If there's an issue reading from the properties file
+     */
     private void initialiseMoveFolders() throws CommandException {
         // Read in properties file
         Properties properties = new Properties();
