58b959a70fed638b324cb26c47aaf9f952c16e09
luiseufrasio <luis.eufrasio@gmail.com>
2022-05-31 11:01:28 -0300

    FISH-6259 enterprise commit

--- appserver/tests/quicklook/ejb/mdb/src/test/mdb/MDBTests.java
+++ appserver/tests/quicklook/ejb/mdb/src/test/mdb/MDBTests.java
@@ -86,7 +86,10 @@ public class MDBTests {
     public void runJMSAppTest(String mdbAppDir) throws Exception {
         String clientJar = cwd + File.separator + mdbAppDir + mdbApp + "Client.jar";
         String gfClientJar = GLASSFISH_HOME + File.separator + "lib" + File.separator + "gf-client.jar";
-        cmd = APPCLIENT + " " + GLASSFISH_APPCLIENT_MAIN_CLASS_NAME
+        final String JDK_VERSION = System.getProperty("java.version");
+        cmd = APPCLIENT
+                + (JDK_VERSION.startsWith("1.8") ? " " : " --add-opens=java.base/java.lang=ALL-UNNAMED ")
+                + GLASSFISH_APPCLIENT_MAIN_CLASS_NAME
                 + " -client " + clientJar
                 + " -targetserver" + " localhost:3700"
                 + " -name ejb-ejb30-hello-mdb-client"
