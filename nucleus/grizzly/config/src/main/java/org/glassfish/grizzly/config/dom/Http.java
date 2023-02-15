a8f1e7097c36f9fe3cae68a945acd00b25687ca5
luiseufrasio <luis.eufrasio@gmail.com>
2023-02-15 12:09:17 -0300

    FISH-6907 : cookie same site properties created

--- nucleus/grizzly/config/src/main/java/org/glassfish/grizzly/config/dom/Http.java
+++ nucleus/grizzly/config/src/main/java/org/glassfish/grizzly/config/dom/Http.java
@@ -75,6 +75,7 @@ public interface Http extends ConfigBeanProxy, PropertyBag {
     boolean XPOWERED_BY = true;
     boolean SERVER_HEADER = true;
     boolean XFRAME_OPTIONS = true;
+    boolean COOKIE_SAME_SITE_ENABLED = false;
     boolean ALLOW_PAYLOAD_FOR_UNDEFINED_HTTP_METHODS = false;
 
     int COMPRESSION_MIN_SIZE = 2048;
@@ -98,6 +99,8 @@ public interface Http extends ConfigBeanProxy, PropertyBag {
     String COMPRESSION_PATTERN = "on|off|force|\\d+";
     String COMPRESSION_STRATEGY = "Default";
     String COMPRESSION_STRATEGY_PATTERN = "Filtered|Default|Huffman Only|\\d+";
+    String COOKIE_SAME_SITE_VALUE = "None";
+    String COOKIE_SAME_SITE_VALUE_PATTERN = "Strict|Lax|None|\\d+";
     String DEFAULT_ADAPTER = "org.glassfish.grizzly.http.server.StaticHttpHandler";
     String URI_ENCODING = "UTF-8";
     String SCHEME_PATTERN = "http|https";
@@ -163,6 +166,11 @@ public interface Http extends ConfigBeanProxy, PropertyBag {
     String getCompressionStrategy();
 
     void setCompressionStrategy(String compressionStrategy);
+    @Attribute(defaultValue = COOKIE_SAME_SITE_VALUE, dataType = String.class)
+    @Pattern(regexp = COOKIE_SAME_SITE_VALUE_PATTERN)
+    String getCookieSameSiteValue();
+
+    void setCookieSameSiteValue(String cookieSameSiteValue);
 
     @Attribute(defaultValue = "" + COMPRESSION_MIN_SIZE, dataType = Integer.class)
     String getCompressionMinSizeBytes();
@@ -454,6 +462,11 @@ public interface Http extends ConfigBeanProxy, PropertyBag {
     String getXframeOptions();
 
     void setXframeOptions(String xframeOptions);
+
+    @Attribute(defaultValue = "" + COOKIE_SAME_SITE_ENABLED, dataType = Boolean.class)
+    String getCookieSameSiteEnabled();
+
+    void setCookieSameSiteEnabled(String enabled);
 
     /**
      * @return <tt>true</tt>, if payload will be allowed for HTTP methods, for
